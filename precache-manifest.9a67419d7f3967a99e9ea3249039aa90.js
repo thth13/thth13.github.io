a306e7101f8b57a888896eb3df37334ad854eb9a
Dmitriy Khutornoy <35405624+thth13@users.noreply.github.com>
2021-01-21 02:06:44 +0200

    Add files via upload

---
+++ precache-manifest.9a67419d7f3967a99e9ea3249039aa90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d59858fa483d07ab5922",
+    "url": "/static/css/main.83f6122b.chunk.css"
+  },
+  {
+    "revision": "d59858fa483d07ab5922",
+    "url": "/static/js/main.d59858fa.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "74b2f2197fc0de2de0d8",
+    "url": "/static/js/2.74b2f219.chunk.js"
+  },
+  {
+    "revision": "b7c1e523a5b15f9e5070c88b5791f534",
+    "url": "/index.html"
+  }
+];
